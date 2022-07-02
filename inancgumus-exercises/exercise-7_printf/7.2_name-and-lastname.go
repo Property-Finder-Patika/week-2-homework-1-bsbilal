9fc3facfeff076fcd0d16d0b2adb76027ff2fa0b
Basulas Bilal <bilal530@gmaill.com>
2022-07-02 11:17:00 +0300

    inancgumus exercises of 7 ok

---
+++ inancgumus-exercises/exercise-7_printf/7.2_name-and-lastname.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("My name is %s and my lastname is %s.\n", "Bilal", "BASULAS")
+
+	msg := "My name is %s and my lastname is %s.\n"
+	fmt.Printf(msg, "Bilal", "BASULAS")
+}
